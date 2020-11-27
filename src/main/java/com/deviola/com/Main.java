cf416b835be194be67751d60c628654556e561a7
Wiola <w.ledwozyw@gmail.com>
2020-11-27 23:49:53 +0100

    added CORS mapping in the Main class

--- src/main/java/com/deviola/com/Main.java
+++ src/main/java/com/deviola/com/Main.java
@@ -2,12 +2,29 @@ package com.deviola.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Main {
 
+    public static final String LOCALHOST_3000 = "http://localhost:3000";
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
 
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/users").allowedOrigins(LOCALHOST_3000);
+                registry.addMapping("/login").allowedOrigins(LOCALHOST_3000);
+                registry.addMapping("/posts").allowedOrigins(LOCALHOST_3000);
+            }
+        };
     }
 }
